38d4908c1ad1fc2d59c1e59a81648e969c04c0bd
Damian <damian.montoya.dm@gmail.com>
2025-03-30 22:10:46 +0200

    Add new animal data to the animals.js file

---
+++ data/animals.js
@@ -0,0 +1,173 @@
+const animals = [
+  {
+    "id": "husky",
+    "animal": "perro",
+    "size": "grande",
+    "name": "Max",
+    "race": "Husky",
+    "description": "Max es un husky lleno de energía que ama correr y jugar al aire libre. Es ideal para familias activas que disfruten de la naturaleza.",
+    "birthdayDate": "2022-09-26T00:00:00Z",
+    "shelter": "Ega Peludos",
+    "character": "Amigable",
+    "like": {
+      "0": "Correr al aire libre",
+      "1": "Jugar con pelotas"
+    },
+    "notLike": {
+      "0": "Los espacios pequeños",
+      "1": "Los ruidos fuertes"
+    },
+  },
+  {
+    "id": "gato_tigre",
+    "animal": "gato",
+    "size": "mediano",
+    "name": "Milo",
+    "race": "Gato común",
+    "description": "Milo es un gatito curioso que disfruta de los días soleados y jugar con juguetes. Necesita un hogar tranquilo para sentirse seguro.",
+    "birthdayDate": "2025-04-15T00:00:00Z",
+    "shelter": "Protectora Animales Etxauri",
+    "character": "aggressive",
+    "like": {
+      "0": "Dormir al sol",
+      "1": "Cazar juguetes"
+    },
+    "notLike": {
+      "0": "El agua",
+      "1": "Los cambios bruscos"
+    },
+  },
+  {
+    "id": "gato_naranja",
+    "animal": "gato",
+    "size": "mediano",
+    "name": "Oliver",
+    "race": "Gato común",
+    "description": "Oliver es un gato tranquilo y cariñoso que disfruta de la estabilidad y los días relajados. Es perfecto para hogares calmados.",
+    "birthdayDate": "2025-02-15T00:00:00Z",
+    "shelter": "Fundación Protectora de Animales",
+    "character": "Amigable",
+    "like": {
+      "0": "Los rascadores",
+      "1": "El agua fresca"
+    },
+    "notLike": {
+      "0": "Cambios en su rutina",
+      "1": "Los espacios ruidosos"
+    },
+  },
+  {
+    "id": "border_collie",
+    "animal": "perro",
+    "size": "grande",
+    "name": "Rocky",
+    "race": "Perro",
+    "description": "Rocky es un perro sociable y activo que ama las aventuras al aire libre. Es ideal para familias con niños y espacios abiertos.",
+    "birthdayDate": "2021-07-26T00:00:00Z",
+    "shelter": "Ega Peludos",
+    "character": "Amigable",
+    "like": {
+      "0": "Correr por la montaña",
+      "1": "Jugar con niños"
+    },
+    "notLike": {
+      "0": "Los sitios abarrotados",
+      "1": "Estar solo"
+    },
+  },
+  {
+    "id": "labrador",
+    "animal": "perro",
+    "size": "grande",
+    "name": "Bella",
+    "race": "Perro",
+    "description": "Bella es una perra mayor y tranquila que disfruta de la calma y el descanso. Busca un hogar donde pueda relajarse y sentirse amada.",
+    "birthdayDate": "2012-02-26T00:00:00Z",
+    "shelter": "Protectora Animales Etxauri",
+    "character": "tranquilo",
+    "like": {
+      "0": "Tomar el sol",
+      "1": "Descansar en lugares cómodos"
+    },
+    "notLike": {
+      "0": "El agua fría",
+      "1": "Los ruidos fuertes"
+    },
+  },
+  {
+    "id": "loro",
+    "animal": "pajaro",
+    "size": "pequeno",
+    "name": "Coco",
+    "race": "Loro",
+    "description": "Coco es un loro divertido y sociable que disfruta de la compañía y aprender nuevos sonidos. Es perfecto para hogares llenos de vida.",
+    "birthdayDate": "2018-09-26T00:00:00Z",
+    "shelter": "Fundación Protectora de Animales",
+    "character": "cariñoso",
+    "like": {
+      "0": "Imitar sonidos",
+      "1": "Comer frutas"
+    },
+    "notLike": {
+      "0": "Estar solo",
+      "1": "El frío"
+    },
+  },
+  {
+    "id": "raton_campo",
+    "animal": "roedor",
+    "size": "pequeño",
+    "name": "Nibbles",
+    "race": "Raton de campo",
+    "description": "Nibbles es un ratón curioso y activo que ama explorar y mantenerse ocupado. Es ideal para quienes buscan una mascota pequeña y entretenida.",
+    "birthdayDate": "2022-04-15T00:00:00Z",
+    "shelter": "Ega Peludos",
+    "character": "Amigable",
+    "like": {
+      "0": "Correr en la rueda",
+      "1": "Explorar su entorno"
+    },
+    "notLike": {
+      "0": "El encierro prolongado",
+      "1": "La inactividad"
+    },
+  },
+  {
+    "id": "gato_persa",
+    "animal": "gato",
+    "size": "mediano",
+    "name": "Luna",
+    "race": "Gato persa",
+    "description": "Luna es una gata persa tranquila que disfruta de la comodidad y el cuidado. Es perfecta para hogares que valoren la calma y el cariño.",
+    "birthdayDate": "2019-11-30T00:00:00Z",
+    "shelter": "Protectora Animales Etxauri",
+    "character": "Amigable",
+    "like": {
+      "0": "Dormir",
+      "1": "Ser cepillado"
+    },
+    "notLike": {
+      "0": "El ruido",
+      "1": "Las sorpresas"
+    },
+  },
+  {
+    "id": "gato_negro",
+    "animal": "gato",
+    "size": "mediano",
+    "name": "Simba",
+    "race": "Gato siamés",
+    "description": "Simba es un gato siamés curioso y juguetón que ama explorar y jugar. Busca un hogar donde pueda mantenerse activo y acompañado.",
+    "birthdayDate": "2020-06-25T00:00:00Z",
+    "shelter": "Protectora Animales Etxauri",
+    "character": "Amigable",
+    "like": {
+      "0": "Explorar",
+      "1": "Jugar con plumas"
+    },
+    "notLike": {
+      "0": "El agua",
+      "1": "Quedarse solo mucho tiempo"
+    },
+  }
+];
